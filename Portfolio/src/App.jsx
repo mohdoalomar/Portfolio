5a582ab7976a6796df9c4574a82b7bac835659e4
Mohdoalomar <mohdoalomar@gmail.com>
2025-01-20 10:17:28 +0300

    deploy

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,5 @@
 
-import Hero from "./components/Hero"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
+
 import Portfolio from "./components/Portfolio"
 function App() {
 
